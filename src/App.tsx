7a8e31a88b467094d34d17eb0d13c45f3cf8f4fa
Chad - ubuntu <scderya@gmail.com>
2020-11-15 23:16:00 +0000

    Add no liability note

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,14 @@ import Calculator from "./calc";
 function App() {
   return (
     <div className="App">
-        <h3 className="App-title" >Duty Calculator</h3>
-        <Calculator />
+      <h3 className="App-title">Duty Calculator</h3>
+      <p>
+        NOTE: This is still work in progress, there may be errors! All
+        information on this site are to be used as a guidelines only. I accept
+        no liability for any damages resulting from the usage of this
+        calculator.
+      </p>
+      <Calculator />
     </div>
   );
 }
