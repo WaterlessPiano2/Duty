b9b2aa8627abb6d14f42b7da0b2ff159cf7cfe4a
Chad - ubuntu <scderya@gmail.com>
2020-11-16 00:50:55 +0000

    Added some links, made warning less intimidating

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,44 @@ function App() {
   return (
     <div className="App">
       <h3 className="App-title">Duty Calculator</h3>
+
+      <div className="result">
+        Please let me know what you think by either filling in the{" "}
+        <a
+          className="link"
+          href="https://forms.gle/Ltko7f1pB4E6dv58A"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          feedback form
+        </a>{" "}
+        or by contacting me on{" "}
+        <a
+          className="link"
+          href="https://www.linkedin.com/in/chadderya/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin!{" "}
+        </a>
+        The Code for this calculator is openly available on{" "}
+        <a
+          className="link"
+          href="https://github.com/WaterlessPiano2/Duty"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github!{" "}
+        </a>
+      </div>
+      <Calculator />
+
       <p>
         NOTE: This is still work in progress, there may be errors! All
         information on this site are to be used as a guidelines only. I accept
         no liability for any damages resulting from the usage of this
         calculator.
       </p>
-      <Calculator />
     </div>
   );
 }
